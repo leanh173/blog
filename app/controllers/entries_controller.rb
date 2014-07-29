ee994480d44040874e1e4c01395c901e3c5ccad4
Le Anh <le.ngoc.anh@framgia.com>
2014-07-29 14:43:15 +0700

    finish blog ver1

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -14,7 +14,7 @@ class EntriesController < ApplicationController
       flash[:success] = "Entry created!"
       redirect_to root_url
     else
-      flash[:error] = "Title/Entry error, please make sure they can not be blank !"
+      flash[:error] = "Title/Entry error!"
       
       redirect_to new_entry_path
     end
